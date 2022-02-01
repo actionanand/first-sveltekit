d58c93d58d2a4d66926b5d7833308306c8e90b65
Anand raja <46064269+actionanand@users.noreply.github.com>
2022-02-01 17:42:08 +0530

    sveltekit routing and scss

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,22 @@
+<script>
+  import Header from '$lib/Header.svelte';
+
+  let date = new Date().getFullYear();
+</script>
+
+
+<Header/>
+<main>
+  <slot/>
+</main>
+
+<footer>
+  &copy; {date}
+</footer>
+
+<style lang="scss">
+  /* Assigning global css */
+  :global(header) {
+    background: wheat;
+  }
+</style>
