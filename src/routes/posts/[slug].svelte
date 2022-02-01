a50fb45004691836edd059ccfdb139dee322ac5e
Anand raja <46064269+actionanand@users.noreply.github.com>
2022-02-01 23:05:24 +0530

    data pre-fetching

--- src/routes/posts/[slug].svelte
+++ src/routes/posts/[slug].svelte
@@ -1,2 +1,22 @@
+<script context="module">
+  export async function load({params}) {
+    const post = {
+      title: params.slug,
+      date: new Date(),
+      body: 'new content is here!'
+    };
+
+    return {
+      props: {
+        post
+      }
+    };
+  }
+</script>
+
+<script>
+  export let post;
+</script>
+
 <!-- /posts/blog -->
-<h3>This is simply slug</h3>
+<h3>This is simply slug. {post.title}</h3>
