a8357dafb0c0adccd25f77b3d643ef04c13a69ca
Anand raja <46064269+actionanand@users.noreply.github.com>
2022-02-02 16:52:39 +0530

    error page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,19 @@
+<script context="module">
+  export function load({error, status}) {
+    return {
+      props: {
+        error,
+        status
+      }
+    };
+  }
+</script>
+
+<script>
+  export let error;
+  export let status;
+</script>
+
+<h1>Error!</h1>
+<h2>{status}</h2>
+<h2>{error.message}</h2>
